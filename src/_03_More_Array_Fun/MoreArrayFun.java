ccab2240e829ca3cdf4d3151e2e4fb12389c14af
Julia <hornedjellyfish@gmail.com>
2020-05-16 11:30:14 -0700

    Firework_Display - finished, More_Array_Fun - unfinished
    day 3

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -4,23 +4,37 @@ import java.util.Random;
 
 public class MoreArrayFun {
 	//1. Create a main method to test the other methods you write.
-	
-	
-	
+		public static void main (String [] args) {
+			String[] s = new String[5];
+			s[1] = "some random words";
+			s[4] = "more random words";
+			printStrings(s);
+			reverseStrings(s);
+		}
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
-	
+			public static void printStrings(String[] strings) {
+				for (int i = 0; i < strings.length; i++) {
+					System.out.println(strings[i]);
+				}
+			}
 	
 	
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
-	
+			public static void reverseStrings(String[] strings) {
+				for (int i = strings.length; i > 0; i--) {
+					System.out.println(strings[i-1]);
+				}
+			}
 	
 	
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
-	
+//			public static void everyOtherString(String[] strings) {
+//				
+//			}
 	
 	//5. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in a completely random order. Almost every run of the program should result in a different order.
 	
-	
+		
 }
